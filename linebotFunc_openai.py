8eaddf90b0407a7057e1b6472a5f85f38c281b78
BuTteR0927 <69560351+BuTteR0927@users.noreply.github.com>
2023-10-25 03:49:12 +0800

    Update linebotFunc_openai.py

--- linebotFunc_openai.py
+++ linebotFunc_openai.py
@@ -27,6 +27,7 @@ def GPT_response(text):
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
