8bc77b44294d56efc0ccb060ee9fdc74dc5a616f
BuTteR0927 <69560351+BuTteR0927@users.noreply.github.com>
2023-10-25 05:20:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,15 +26,6 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
     # 重組回應
     answer = response['choices'][0]['text'].replace('。','')
     return answer '''
-def GPT_response(text):
-    # 接收回应
-    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
-    if 'error' in response:
-        print(f"OpenAI error: {response['error']['message']}")
-        return '发生错误，请检查日志以获取更多信息'
-    # 重组回应
-    answer = response['choices'][0]['text'].replace('。','')
-    return answer
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -51,7 +42,7 @@ def callback():
         abort(400)
     return 'OK'
 
-
+'''
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -64,7 +55,7 @@ def handle_message(event):
         print(f"An error occurred: {str(e)}")
         line_bot_api.reply_message(event.reply_token, TextSendMessage('发生错误，请检查日志以获取更多信息'))
 
-'''
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
@@ -74,7 +65,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
     except:
         line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
-'''
+
 
 @handler.add(PostbackEvent)
 def handle_message(event):
@@ -88,7 +79,77 @@ def welcome(event):
     name = profile.display_name
     message = TextSendMessage(text=f'{name}歡迎加入')
     line_bot_api.reply_message(event.reply_token, message)
-    
+'''
+
+baseurl = 'https://ef99-61-220-181-211.ngrok.io/static/'  # 静态文件的网址
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    mtext = event.message.text
+    if mtext == '@傳送聲音':
+        try:
+            message = AudioSendMessage(
+                original_content_url=baseurl + 'rickroll.MP3',  # 声音文件位于static文件夹
+                duration=20000  # 声音长度为20秒
+            )
+            line_bot_api.reply_message(event.reply_token, message)
+        except:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤!'))
+    elif mtext == '@傳送影片':
+        try:
+            message = VideoSendMessage(
+                original_content_url=baseurl + 'rickroll.mp4',  # 视频文件位于static文件夹
+                preview_image_url=baseurl + 'eggdog.jpeg'
+            )
+            line_bot_api.reply_message(event.reply_token, message)
+        except:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤!'))
+    elif mtext == '@傳送貼圖':
+        try:
+            message = StickerSendMessage(package_id='6362', sticker_id='11087922')
+            line_bot_api.reply_message(event.reply_token, message)
+        except:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤!'))
+    elif mtext == '@多項傳送':
+        try:
+            message = [
+                StickerSendMessage(package_id='6632', sticker_id='11825377'),
+                TextSendMessage(text="這是pizza圖片!"),
+                ImageSendMessage(
+                    original_content_url="https://i.imgur.com/0ooelxA.jpeg",
+                    preview_image_url="https://i.imgur.com/0ooelxA.jpeg"
+                )
+            ]
+            line_bot_api.reply_message(event.reply_token, message)
+        except:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤!'))
+    elif mtext == '@傳送位置':
+        try:
+            message = LocationSendMessage(
+                title='仙跡岩自然步道入口',
+                address='116台北市文山區木柵',
+                latitude=24.99388888888889,  # 緯度
+                longitude=121.55825  # 經度
+            )
+            line_bot_api.reply_message(event.reply_token, message)
+        except:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤!'))
+    elif mtext == '@快速選單':
+        try:
+            message = TextSendMessage(
+                text='請選擇最喜歡的程式語言',
+                quick_reply=QuickReply(
+                    items=[
+                        QuickReplyButton(action=MessageAction(label="Python", text="Python")),
+                        QuickReplyButton(action=MessageAction(label="Java", text="Java")),
+                        QuickReplyButton(action=MessageAction(label="C#", text="C#")),
+                        QuickReplyButton(action=MessageAction(label="Basic", text="Basic"))
+                    ]
+                )
+            )
+            line_bot_api.reply_message(event.reply_token, message)
+        except:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤!'))
 
 import os
 if __name__ == "__main__":
